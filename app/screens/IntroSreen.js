593192d51020842efcef6534b0be8b9326b9df71
mitulsavani <mitul.savani2@yahoo.com>
2018-02-26 23:47:29 -0800

    Logo and two buttons added on IntroScreen

--- app/screens/IntroSreen.js
+++ app/screens/IntroSreen.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button} from 'react-native';
 
 export default class IntroScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>IntroScreen</Text>
+        <View style={styles.imageContainer}>
+          <Image style={styles.dauglogo} source = {require('../Images/daugLogo.png')} />
+         </View>
+        <View style={styles.joinus}>
+            <View style={styles.login}>
+            <Button onPress={() => this.changeBackground()}
+              title="Login"
+              color= 'white' />
+            </View>
+            <View style={styles.signup}>
+            <Button onPress={() => this.changeBackground()}
+              title="Sign Up"
+              color= 'white'
+              size = "30" />
+            </View>
+        </View>
       </View>
     );
   }
@@ -14,8 +29,40 @@ export default class IntroScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 8,
+    width: 375,
+    backgroundColor: '#ED8071',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+  dauglogo: {
+    width: 200,
+    height: 200,
+  },
+  joinus: {
+    flex: 1,
+    flexDirection: 'row',
+    width: 375,
+    backgroundColor: '#A0545B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  
+  },
+  login: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signup:
+  {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
