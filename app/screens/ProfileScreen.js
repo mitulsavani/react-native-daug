d6b76ffbaf25d354d1ce282cb3e7280252529dbc
mitulsavani <mitul.savani2@yahoo.com>
2018-03-03 14:47:28 -0800

    Finished with the ProfileScreen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -1,37 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View, Vibration, Image } from 'react-native';
+import { StyleSheet, Text, View, Vibration, Image, ScrollView } from 'react-native';
 import { MaterialCommunityIcons, SimpleLineIcons, Entypo } from '@expo/vector-icons';
 import { Button } from 'react-native-elements';
 
-import DAUG_Cover from '../../assets/dogCover.jpg';
+import Rocco_Cover from '../../assets/dogCover.jpg';
+import Rocco_DisplayPic from '../../assets/Rocco_displayPic.jpg';
 
 export default class ProfileScreen extends React.Component {
   render() {
     return (
+    
       <View style={styles.mainContainer}>
 
           <View style={styles.daugCoverPic}>
-              <Image style={styles.dauglogo} source = {DAUG_Cover} />
+              <Image style={styles.dauglogo} source = {Rocco_Cover} />
           </View>
 
           <View style={styles.infoContainer}>
               <View style={styles.logs}>
                   <View style = { styles.displayPic } >
-                      <Text>display</Text>
+                      <Image style={styles.roccoDisplayPic} source = {Rocco_DisplayPic} />
                   </View>
-                  <View Style = { styles.activity } >
-                      <Text>activity</Text>
+                  <View style = { styles.activityContainer } >
+                          
+                          <View style = { styles.postAndFollowContainer} >
+                              <View style = {styles.posts} >
+                                  <Text style = {{fontWeight: 'bold'}}> 
+                                      3 
+                                  </Text>
+                                  <Text style = {{fontWeight: 'bold'}}>
+                                      Post 
+                                  </Text>
+                              </View>
+                              <View style = {styles.followers} >
+                                  <Text style = {{fontWeight: 'bold'}}> 
+                                      501 
+                                  </Text>
+                                  <Text style = {{fontWeight: 'bold'}}> 
+                                      Followers 
+                                  </Text>
+                              </View>
+                              <View style = {styles.following} >
+                                  <Text style = {{fontWeight: 'bold'}}> 
+                                      99 
+                                  </Text>
+                                  <Text style = {{fontWeight: 'bold'}}> 
+                                      Following 
+                                  </Text>
+                              </View>
+
+                          </View>
+
+                          <View style = { styles.editProfileContainer} >
+                                <View style={styles.editButton}>
+                                    <Button
+                                      text='Edit Profile'
+                                      textStyle={{ fontSize: 12, color: 'black'}}
+                                      buttonStyle={{
+                                        backgroundColor: 'white',
+                                        width: 100,
+                                        height: 20,
+                                        borderColor: 'black',
+                                        borderWidth: 1,
+                                        borderRadius: 5
+                                      }}
+                                      icon={
+                                        <MaterialCommunityIcons
+                                          name='account-edit'
+                                          size={20}
+                                          color='black'
+                                        />
+                                      }
+                                      onPress = {() => this.onSubmitButtonPressed()}
+                                    />
+                                </View>
+                          </View>
                   </View>
               </View>
 
-              <View style={styles.bioData}>
-                   <View style={styles.Name}>
-                    <Text>Bio</Text>
-                  </View><View style={styles.status}>
-                    <Text>Bio</Text>
+              
+              <View style = { styles.bioData } >
+                  <View style = { styles.name } >
+                          <Text style = {{fontSize: 25, fontWeight: 'bold'}}>
+                              Rocco
+                          </Text>
+                  </View>
+                  <View style = { styles.status } >
+                          <Text style = {{fontSize: 15, fontStyle: 'italic'}}>
+                              I am small, inconent, and lovable dog 🐶
+                          </Text>
                   </View>
               </View>
-          
           </View>
 
           <View style={styles.logOutContainer}>
@@ -64,6 +123,7 @@ export default class ProfileScreen extends React.Component {
 }
 
 const styles = StyleSheet.create({
+
   mainContainer: {
     flex: 1,
     flexDirection: 'column',
@@ -96,15 +156,19 @@ const styles = StyleSheet.create({
   },
   bioData: {
     flex: 1.5,
-    backgroundColor: 'blue'
+    backgroundColor: 'white',
+    borderBottomWidth: 1,
+    borderColor: '#aaaaaa'
   },
   displayPic: {
     flex: 1,
     backgroundColor: 'white',
+    alignItems: 'center',
   },
-  activity: {
-    flex: 1,
-    backgroundColor: 'black'
+  activityContainer: {
+    flex: 3,
+    flexDirection: 'column',
+    backgroundColor: 'white'
   },
   dauglogo: {
     width: 400,
@@ -112,11 +176,50 @@ const styles = StyleSheet.create({
     marginLeft: 25
   },
   name: {
-    flex: 1,
-    backgroundColor: 'red'
+    flex: 1.5,
+    backgroundColor: 'white',
+    marginLeft: 15,
+    justifyContent: 'center'
   },
   status: {
     flex: 1,
-    backgroundColor: 'wheat'
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    marginLeft: 15
+  },
+  roccoDisplayPic: {
+    height:80,
+    width: 80,
+    borderRadius: 40,
+    marginTop: -20
+  },
+  postAndFollowContainer: {
+    flex: 1.5,
+    flexDirection: 'row',
+  },
+  editProfileContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  editButton: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  posts :{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  followers: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  following: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
